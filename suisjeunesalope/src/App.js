df21ba9ecebf9323ccb9fea46c67a9adaf681e18
Clément Parmentier <clement.parmentier@mic-belgique.be>
2020-11-18 17:18:17 +0100

    Ouh la salope

--- suisjeunesalope/src/App.js
+++ suisjeunesalope/src/App.js
@@ -1,49 +1,129 @@
-// import logo from './logo.svg';
-// import 'react';
-// import * as canvas from 'canvas';
-// import * as faceapi from 'face-api.js';
-// import './App.css';
+import logo from './logo.svg';
+import React from 'react';
+import * as canvas from 'canvas';
+import * as faceapi from 'face-api.js';
+import './App.css';
 
-// const App = (props) => {
+const App = (props) => {
 
-//   const [Canvas, setCanvas] = React.useState();
-//   const [Image, setImage] = React.useState();
-//   const [ImageData, setImageData] = React.useState();
+  const [Canvas, setCanvas] = React.useState();
+  const [Image, setImage] = React.useState();
+  const [ImageData, setImageData] = React.useState();
+  const [Done, setDone] = React.useState(false);
+  const [LabeledFaceDescriptors, setLabeledFaceDescriptors] = React.useState();
+  const [ready, setReady] = React.useState(false);
+  const [DrawBox, setDrawBox] = React.useState();
+  const [Width, setWidth] = React.useState(500);
+  const [Height, setHeight] = React.useState(500);
 
+  React.useEffect(async () => {
+    setCanvas(canvas.Canvas);
+    setImage(canvas.Image);
+    setImageData(canvas.ImageData);
 
-//   React.useEffect(() => {
-//     setCanvas(canvas.Canvas);
-//     setImage(canvas.Image);
-//     setImageData(canvas.ImageData);
+    //faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+    faceapi.env.monkeyPatch({
+      Canvas: HTMLCanvasElement,
+      Image: HTMLImageElement,
+      ImageData: ImageData,
+      Video: HTMLVideoElement,
+      createCanvasElement: () => document.createElement('canvas'),
+      createImageElement: () => document.createElement('img')
+    });
 
-//     faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+    await faceapi.loadFaceRecognitionModel('/Models');
+    await faceapi.loadSsdMobilenetv1Model('/Models');
+    await faceapi.loadFaceLandmarkModel('/Models');
+    await faceapi.loadFaceExpressionModel('/Models');
 
-//     let faceData = await fetch(`/facedata/salope.json`).then(r => r.json());
-//     await faceapi.loadFaceRecognitionModel('');
+    //let faceData = await fetch(`/facedata/salope.json`).then(r => r.json());
 
-//     labeledFaceDescriptors = faceData.map(desc => {
-//       let arr = desc.faceDescriptors.map(fd => {
-//         return new Float32Array([...Object.values(fd)]); 
-//       });
-//       return new faceapi.LabeledFaceDescriptors(desc.label, arr);
-//     });
+    var descriptor;
+    var labeledFaceDescriptors;
+    let descuri = 'desc.json';
+    fetch("desc.json")
+      .then(response => response.json())
+      .then(json => {
+        descriptor = new Float32Array(Object.values(json))
+        labeledFaceDescriptors = new faceapi.LabeledFaceDescriptors('Une Salope', [descriptor]);
+        console.log(labeledFaceDescriptors);
+        setLabeledFaceDescriptors([labeledFaceDescriptors]);
+        setReady(true);
+      });
+  }, [])
 
-//     console.log(labeledFaceDescriptors[0]);
+  React.useEffect(() => {
+    DrawBox && DrawBox.draw(canvas);
+    console.log("test");
+  }, [DrawBox])
 
-//     ready = true;
+  const loadImage = (event) => {
+    if (!ready) {
+      alert("Models and configuration not loaded yet, please wait a minute");
+      return;
+    }
+    let canvas = document.querySelector("canvas");
+    let ctx = canvas.getContext("2d");
+    let file = event.target;
+    // if (!props.file) {
+    //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //   return;
+    // }
+    console.log(`file : ${file}`)
+    let reader = new FileReader();
+    reader.onload = function (event) {
+      let img = document.createElement('img');
+      img.onload = async function () {
+        let imageRatio = img.width / img.height;
+        canvas.height = 500;
+        canvas.width = 500 * imageRatio;
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+        let input = img;
+        let fullFaceDescriptions = await faceapi
+          .detectAllFaces(input)
+          .withFaceLandmarks()
+          .withFaceDescriptors()
+          .withFaceExpressions();
+        let dim = new faceapi.Dimensions(canvas.width, canvas.height);
+        fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions, dim);
+        console.log(`fullFaceDescriptions : ${fullFaceDescriptions}`);
+        if (!props.recognition) faceapi.draw.drawDetections(canvas, fullFaceDescriptions);
+        if (!props.recognition && props.showLandmarks) faceapi.draw.drawFaceLandmarks(canvas, fullFaceDescriptions);
+        if(!props.recognition && props.showExpressions) faceapi.draw.drawFaceExpressions(canvas, fullFaceDescriptions);
+        if (props.recognition) {
+          const maxDescriptorDistance = 0.6;
+          const faceMatcher = new faceapi.FaceMatcher(LabeledFaceDescriptors, maxDescriptorDistance);
 
-//   }, [])
+          const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor));
+          results.forEach((bestMatch, i) => {
+            const box = fullFaceDescriptions[i].detection.box;
+            const text = bestMatch.toString();
+            const drawBox = new faceapi.draw.DrawBox(box, { label: text });
+            setDrawBox(drawBox);
+          });
+          setDone(true);
+        }
+      }
+      img.src = event.target.result;
+    }
+    if (file) {
+      console.log(file)
+      reader.readAsDataURL(file.files[0]);
+    }
+  }
 
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Testez une image :
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Testez une image :
+        </p>
+        <input type="file" id="analyze" onChange={loadImage} />
+        <canvas width={Width} height={Height} id="canvas" />
+      </header>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
--- suisjeunesalope/src/index.js
+++ suisjeunesalope/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Training from './Training';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Training />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
