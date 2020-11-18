0d8b9fc23c4a405fa2c0b81a51e768673da5f122
Clément Parmentier <clement.parmentier@mic-belgique.be>
2020-11-18 15:15:21 +0100

    ibra desc created

--- suisjeunesalope/src/Training.js
+++ suisjeunesalope/src/Training.js
@@ -2,14 +2,11 @@ import logo from './logo.svg';
 import * as React from 'react';
 import * as canvas from 'canvas';
 import * as faceapi from 'face-api.js';
-import sharp from "sharp";
 
 import './App.css';
-import "@tensorflow/tfjs-node";
-import nodeCanvas from "canvas";
+// import "@tensorflow/tfjs-node";
 import fs from "fs";
 import fetch from "node-fetch";
-import util from "util";
 import stream from "stream";
 
 const Training = (props) => {
@@ -24,37 +21,43 @@ const Training = (props) => {
     setImage(canvas.Image);
     setImageData(canvas.ImageData);
 
-    faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+    //faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+    faceapi.env.monkeyPatch({
+      Canvas: HTMLCanvasElement,
+      Image: HTMLImageElement,
+      ImageData: ImageData,
+      Video: HTMLVideoElement,
+      createCanvasElement: () => document.createElement('canvas'),
+      createImageElement: () => document.createElement('img')
+      })
 
     await faceapi.loadFaceRecognitionModel('');
+    await faceapi.loadSsdMobilenetv1Model('');
+    await faceapi.loadFaceLandmarkModel('');
 
 
-    //Choper l'image
     let uri = "bibi.png";
     let filename = uri.split("/");
-    filename = filename[filename.length - 1];
-    let fileExtension = filename.substr(filename.lastIndexOf(".") + 1);
-    if (filename === fileExtension) {
-      fileExtension = "jpg";
-      filename = `${filename}.${fileExtension}`;
-    }
-    filename = `tmp/${filename}`;
-    //await downloadImage(uri, filename);
 
-    //resize l'image
-    console.log(`Image was downloaded, resizing`);
-    let modifiedFilename = filename.replace(`.${fileExtension}`, `__resized.jpg`);
-    await sharp(filename)
-      .resize({ width: STD_SIZE, height: STD_SIZE, fit: "cover" })
-      .jpeg()
-      .toFile(modifiedFilename)
-      .catch(e => console.log("Error transforming image", e));
-    console.log(`Image resized to ${STD_SIZE}x${STD_SIZE} for handle id`)
 
-    //Créer le descriptor
+    // //Créer le descriptor
     // console.log(`Analysing face descriptors`);
     // let descriptors = await getDescriptors(filename);
-    // console.log(`Analysis completed. Cleaning up and sending data to DB`);
+    // console.log(`Descriptor : ${descriptors}`);
+    // let jsondesc = JSON.stringify(descriptors);
+
+    // console.log("BLBLBLBLBL");
+    // console.log(jsondesc);
+
+    var descriptors;
+    let descuri = 'desc.json';
+    fetch("desc.json")
+      .then(response => response.json())
+      .then(json => {
+        descriptors = new Float32Array(Object.values(json)); 
+        console.log(`desc : ${descriptors}`);
+      });
+
 
     //Sauver
     //console.log(descriptors);
@@ -68,22 +71,9 @@ const Training = (props) => {
 
   }, [])
 
-  const downloadImage = async (uri, filename) => {
-    const streamPipeline = util.promisify(stream.pipeline);
-    const response = await fetch(uri);
-    if (response.ok) {
-      return streamPipeline(response.body, fs.createWriteStream(filename));
-    }
-
-    if (response.status === 404) {
-      return fs.copyFileSync(`${__dirname}/assets/default_profile.png`, filename);
-    }
-
-    throw new Error(`Unexpected response ${response.status} ${response.statusText}`);
-  };
-
   const getDescriptors = async filename => {
-    const input = await nodeCanvas.loadImage(filename);
+    
+    const input = await canvas.loadImage(filename);
 
     const fullFaceDescription = await faceapi.detectSingleFace(input).withFaceLandmarks().withFaceDescriptor();
 
